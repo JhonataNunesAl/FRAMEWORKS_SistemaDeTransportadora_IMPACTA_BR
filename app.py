88fc843f76825d2671cf527900302b0b173fcbdf
Joao Manoel Lelis Oliveira <2401807@SALAS.AULAS>
2025-09-05 11:13:54 -0300

    Configurando API com migrations

---
+++ app.py
@@ -0,0 +1,19 @@
+from config import app, db
+from flask_migrate import Migrate, upgrade
+from flask import jsonify
+
+from view.apartamento_view import apartamento_blueprint
+
+migrate = Migrate(app, db)
+
+app.register_blueprint(apartamento_blueprint, url_prefix='/api/apartamentos')
+
+with app.app_context():
+    upgrade()
+
+if __name__ == '__main__':
+    app.run(
+        host=app.config['HOST'],
+        port=app.config['PORT'],
+        debug=app.config['DEBUG']
+    )
