210d2d1d62b7b410b11a8d136cbf1ee1a9917052
Joao Manoel Lelis Oliveira <2401807@SALAS.AULAS>
2025-09-05 11:29:56 -0300

    Ajuste para da tabla de apartamento

--- model/apartamento_model.py
+++ model/apartamento_model.py
@@ -4,10 +4,10 @@ from config import db
 class Apartamento(db.Model):
     __tablename__ = "Apartamentos"
 
-    Numero_AP = db.Column(db.String(20), prymary_key = True)
-    Ocupado = db.Column(db.Boolean, default = False)
-    Alugado = db.Column(db.Boolean, default = False)
-    Venda = db.Column(db.Boolean, default = False)
+    Numero_AP = db.Column(db.String(20), prymary_key=True)
+    Ocupado = db.Column(db.Boolean, default=False)
+    Alugado = db.Column(db.Boolean, default=False)
+    Venda = db.Column(db.Boolean, default=False)
 
 
     morador_id = db.Column(db.Integer, db.ForegeinKey("Moradores.id"))
