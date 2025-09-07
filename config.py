5a35219dc532c46c1da16fb44a289a59ba1a4fb6
Joao Manoel Lelis Oliveira <lelis,jm5@gmail.com>
2025-09-07 20:19:22 -0300

    Adicionando a conexão com o banco

--- config.py
+++ config.py
@@ -12,7 +12,10 @@ app.config['PORT'] = 8000
 app.config['DEBUG'] = True
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+    "DATABASE_URL",
+    "postgresql://databaseapartamento_user:H6PRvbtRhA9n5L94yBXyIOfV5gGgYmI7@dpg-d2tckgeuk2gs73cof7f0-a.oregon-postgres.render.com/databaseapartamento"
+)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
