cab87b5a505aaedb8f365021df46ad3bef1112f5
Joao Manoel Lelis Oliveira <lelis,jm5@gmail.com>
2025-09-07 20:02:33 -0300

    Fix: corrigido primary_key e ForeignKey em apartamento_model

--- model/apartamento_model.py
+++ model/apartamento_model.py
@@ -4,13 +4,13 @@ from config import db
 class Apartamento(db.Model):
     __tablename__ = "Apartamentos"
 
-    Numero_AP = db.Column(db.String(20), prymary_key = True)
+    Numero_AP = db.Column(db.String(20), primary_key = True)
     Ocupado = db.Column(db.Boolean, default = False)
     Alugado = db.Column(db.Boolean, default = False)
     Venda = db.Column(db.Boolean, default = False)
 
 
-    morador_id = db.Column(db.Integer, db.ForegeinKey("Moradores.id"))
+    morador_id = db.Column(db.Integer, db.ForeignKey("Moradores.id"))
     morador = db.relationship("morador", back_populates="Apartamentos")
     
     def __repr__(self):
