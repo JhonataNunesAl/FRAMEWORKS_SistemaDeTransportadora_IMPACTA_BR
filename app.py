9bc08161311c83c27a1e02c82443fa0cce1f6f65
Joao Manoel Lelis Oliveira <lelis,jm5@gmail.com>
2025-09-29 01:21:40 -0300

    adicionando os blueprints de morador

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from config import app, db
 from flask_migrate import Migrate, upgrade
 
 from view.apartamento_view import apartamento_blueprint
+from view.morador_view import morador_blueprint
 
 migrate = Migrate(app, db)
 
 app.register_blueprint(apartamento_blueprint, url_prefix='/api')
+app.register_blueprint(morador_blueprint, url_prefix='/api')
 
 with app.app_context():
     upgrade()
