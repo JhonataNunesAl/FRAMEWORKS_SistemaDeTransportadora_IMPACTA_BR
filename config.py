c066c76a7eaa36720a2302368dbef7b01c152c0b
Joao Manoel Lelis Oliveira <lelis,jm5@gmail.com>
2025-09-29 01:52:44 -0300

    Ajuste no config sobre a conexão com o banco de dados

--- config.py
+++ config.py
@@ -11,11 +11,19 @@ app.config['HOST'] = '0.0.0.0'
 app.config['PORT'] = 8000
 app.config['DEBUG'] = True
 
-
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv(
+database_url = os.getenv(
     "DATABASE_URL",
     "postgresql://databaseapartamento_dyo1_user:nNN1iEvn0cNmqcDG4aHFxKhG8yZlOSw0@dpg-d3d0ss0gjchc739jp9o0-a.oregon-postgres.render.com/databaseapartamento_dyo1"
 )
+
+if database_url.startswith("postgres://"):
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+
+if "sslmode" not in database_url:
+    database_url += "?sslmode=require"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
